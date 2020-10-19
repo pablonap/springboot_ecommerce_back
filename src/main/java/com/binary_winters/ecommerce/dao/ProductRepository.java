f70f194bf825b15a7607f50260965dc7e2268352
chipo <pnapoli33@gmail.com>
2020-10-19 15:32:26 -0300

    video #79 - Add query method to get list of products based on the category id.

--- src/main/java/com/binary_winters/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/binary_winters/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.binary_winters.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.binary_winters.ecommerce.entity.Product;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	// This allows go to: localhost:8080/api/products/search/findByCategoryId?id=1
+	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
