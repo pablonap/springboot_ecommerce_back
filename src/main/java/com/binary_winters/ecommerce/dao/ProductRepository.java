9a0049d0f978a52a95de22462f8ebf6cba81840f
chipo <pnapoli33@gmail.com>
2020-10-20 14:41:08 -0300

    video #92.0 - Add findByNameContaining method in ProductRepository.

--- src/main/java/com/binary_winters/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/binary_winters/ecommerce/dao/ProductRepository.java
@@ -12,6 +12,10 @@ import com.binary_winters.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	// This allows go to: localhost:8080/api/products/search/findByCategoryId?id=1
+	// SELECT * FROM product where product.category_id=1;
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
+	// select * from product p where p.name LIKE CONCAT('%', 'JavaScript Cookbook', '%')
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
